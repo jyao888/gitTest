aadf912213c2a92ea1422c9e7e3ba86d3b11baa9
jyao <jyao2022@126.com>
2022-06-14 15:16:40 +0800

    first commit

---
+++ src/main/java/com/junyao/gitTest.java
@@ -0,0 +1,11 @@
+package com.junyao;
+
+/**
+ * @author wjy
+ * @create 2022-06-14 15:13
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
